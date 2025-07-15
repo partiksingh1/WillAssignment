875deba3811744eee6c8915b08c93d4437f98825
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-07-15 14:37:22 +0200

    feat : add steps for the process

---
+++ src/types/steps.ts
@@ -0,0 +1,13 @@
+export const steps: string[] = [
+    'Testator Details',
+    'Beneficiaries',
+    'Bank Accounts',
+    'Insurance Policies',
+    'Stocks',
+    'Mutual Funds',
+    'Jewellery',
+    'House Properties',
+    'Land Properties',
+    'Executors & Guardianship',
+    'Review & Generate Will',
+];
